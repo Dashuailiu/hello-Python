1a51433d630e97540b86c709bb272dd8855a16f1
Dashuailiu <583036108@qq.com>
2016-05-06 15:13:03 +0800

    socket_tcp

---
+++ socket/demo_socket_sever.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+import socket
+import threading
+from threading import Thread
+
+def tcp_link(sock,addr):
+    sock.send(b'hello I`m Sever!')
+    while True:
+        data = sock.recv(1024)
+        if data.decode('utf-8') == 'exit' or not data:
+            break
+        else:
+            sock.send(('u input %s' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+s.bind(('127.0.0.1',9999))
+s.listen(5)
+
+while True:
+    sock,addr = s.accept()
+    t = threading.Thread(target=tcp_link,args=(sock,addr))
+    t.start()
